e7c8be5b4706db54a63c6c666dd832e76e945a53
Phil Derome <phderome@gmail.com>
2017-01-28 20:10:46 -0500

    fixed log for getSessionizedCounts, made getKeySessions writer-monadic, print log at the end, and refactored a little formatting of aggregate Made sbt assembly work with the cats dependency.

--- build.sbt
+++ build.sbt
@@ -4,10 +4,13 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+mainClass in assembly := Some("phderome.challenge.WeblogChallenge")
+
+assemblyJarName in assembly := "paytmphderome_2.11-1.0.jar"
+
 // installed spark 2.1.0 (with hadoop 2.7.2) locally on computer
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.1.0",
-  "org.apache.spark" %% "spark-sql" % "2.1.0",
-  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
-  "org.typelevel" %% "cats-core" % "0.4.0"
-)
+  "org.apache.spark" %% "spark-core" % "2.1.0" % Provided,
+  "org.apache.spark" %% "spark-sql" % "2.1.0"  % Provided,
+  "org.scalacheck" %% "scalacheck" % "1.13.4" % Test,
+  "org.typelevel"    %% "cats"       % "0.7.2" )
--- src/main/scala/phderome/challenge/WeblogChallenge.scala
+++ src/main/scala/phderome/challenge/WeblogChallenge.scala
@@ -7,11 +7,9 @@ import cats.data.Writer
 import org.apache.spark.sql.{Dataset, _}
 
 object WeblogChallenge {
-
   implicit class toW(s: String) {
     def ~>[A](ds: Dataset[A]): Writer[List[String], Dataset[A]] = Writer(s :: Nil, ds)
     def ~>[K, V](kvgd: KeyValueGroupedDataset[K, V]): Writer[List[String], KeyValueGroupedDataset[K, V]] = Writer(s :: Nil, kvgd)
-
   }
 
   case class Pipeline(engagedClients: Dataset[ClientDurationAverage],
@@ -32,7 +30,7 @@ object WeblogChallenge {
       .write
       .mode(SaveMode.Overwrite)
       .save(path = params.path)
-    params.ds.take(params.take).foreach { s => println(s"*************************  ${params.printHeader}: $s") }
+    params.ds.take(params.take).foreach { s => println(s"${params.printHeader}: $s") }
   }
 
   def aggregate[T](ds: Dataset[T],
@@ -40,15 +38,16 @@ object WeblogChallenge {
                    one: T => String,
                    reduce: Dataset[T] => String): String = {
     ds.count() match {
-      case 0 => zero //"Not enough data\n"
-      case 1 => one(ds.take(1).apply(1)) //._2 f"$avgUrl%d 1 => avg unique per session $avgUrl%s\n"
+      case 0 => zero
+      case 1 => one(ds.take(1).apply(1))
       case _ => reduce(ds)
     }
   }
 
-  def getSessionized(fileName: String, windowAsNanos: Long)(implicit spark: SparkSession): Writer[List[String], Dataset[SessionWindow]] = {
+  def getSessionized(fileName: String, windowAsNanos: Long)
+                    (implicit spark: SparkSession): Writer[List[String], Dataset[SessionWindow]] = {
     import spark.implicits._
-    "getSessionized" ~> spark.read.textFile(fileName)
+    s"(getSessionized $fileName $windowAsNanos) " ~> spark.read.textFile(fileName)
       .flatMap(toClientAccess)
       .groupByKey(_.client)
       .mapValues(datum => ClientAttributes(datum.attributes.epochNanosecs, datum.attributes.uri))
@@ -58,14 +57,15 @@ object WeblogChallenge {
   def getDurationAndSize(keyedSessionWindows: KeyValueGroupedDataset[SessionKey, SessionWindow])
                         (implicit spark: SparkSession): Writer[List[String], Dataset[ClientDurationAndSize]] = {
     import spark.implicits._
-    "getDurationAndSize" ~> keyedSessionWindows
+    "(getDurationAndSize) " ~> keyedSessionWindows
       .flatMapGroups((k, sWindows) => sWindows.flatMap(w => getDurationAndSizeBySessionWithClient(k, w.data)))
       .map { case (k,v) => ClientDurationAndSize(k, v) }
   }
 
-  def getEngagedClients(ds: Dataset[ClientDurationAndSize])(implicit spark: SparkSession): Writer[List[String], Dataset[ClientDurationAverage]] = {
+  def getEngagedClients(ds: Dataset[ClientDurationAndSize])
+                       (implicit spark: SparkSession): Writer[List[String], Dataset[ClientDurationAverage]] = {
     import spark.implicits._
-    "getEngagedClients" ~> ds
+    "(getEngagedClients) " ~> ds
       .groupByKey(_.client) // consolidate/key by client and obtain iterable of (client, durations)
       .mapValues(_.durationAndSize.duration) // weed out client to keep only duration in iterables
       .mapGroups((client, durs) => createCDT(client, durs.toArray))
@@ -76,15 +76,19 @@ object WeblogChallenge {
   def getSessionizedCounts(ksws: KeyValueGroupedDataset[SessionKey, SessionWindow])
                           (implicit spark: SparkSession): Writer[List[String], Dataset[(String, Long)]] = {
     import spark.implicits._
-    "getEngagedClients" ~> ksws
-      .flatMapGroups((k, sWindows) => sWindows.map(getCount))
+    "(getSessionizedCounts)  " ~> ksws.flatMapGroups((k, sWindows) => sWindows.map(getCount))
   }
 
   def getUniqueUrlPerSessions(ksws: KeyValueGroupedDataset[SessionKey, SessionWindow])
                              (implicit spark: SparkSession): Writer[List[String], Dataset[(Long, Long)]] = {
     import spark.implicits._
-    "getEngagedClients" ~> ksws
-      .flatMapGroups((k, sWindows) => sWindows.map(getUniqueUrlsBySession))
+    "(getUniqueUrlPerSessions) " ~> ksws.flatMapGroups((k, sWindows) => sWindows.map(getUniqueUrlsBySession))
+  }
+
+  def getKeySessions(ds: Dataset[SessionWindow])
+                    (implicit spark: SparkSession): Writer[List[String], KeyValueGroupedDataset[SessionKey, SessionWindow]] = {
+    import spark.implicits._
+    "(getKeySessions) " ~> ds.groupByKey(_.sessionKey)
   }
 
   def main(args: Array[String]) {
@@ -107,39 +111,37 @@ object WeblogChallenge {
 
     val pipeline = for {
       sessionized <- getSessionized(args(0), windowTimeSpanAsNanos)(spark)
-      keyedSessionWindows = sessionized.groupByKey(_.sessionKey)
+      keyedSessionWindows <- getKeySessions(sessionized)
       durationAndSizesPerSessionsDS <- getDurationAndSize(keyedSessionWindows)
-      engagedClients <- getEngagedClients(durationAndSizesPerSessionsDS)
       sessionizedCounts <- getSessionizedCounts(keyedSessionWindows)
       uniqueUrlPerSessions <- getUniqueUrlPerSessions(keyedSessionWindows)
+      engagedClients <- getEngagedClients(durationAndSizesPerSessionsDS)
     } yield Pipeline(engagedClients, sessionizedCounts, uniqueUrlPerSessions, durationAndSizesPerSessionsDS )
-
-    val (theLog, theDatasets) = pipeline.run
-    println(s"Pipeline Log is $theLog}")
+    val (theLog, theDatasets) = pipeline.run // executes the Writer Monad (so Log is available) but not the Spark pipeline yet.
 
     // Spark actions start below, after having done required transformations.
     val uniqueUrlLine: String = {
-      def reduceTwoPlus(ds: Dataset[(Long, Long)]): String = {
-        val (sessionCount: Long, urlCount: Long) = ds.reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+      def format(urlCount: Long, sessionCount: Long): String = {
         val avgUrl = urlCount.toDouble/sessionCount.toDouble
         f"$urlCount%d $sessionCount%d => avg unique per session $avgUrl%4.4f\n"
       }
-      def reduceOne(pair: (Long, Long)): String = {
-        val avgUrl: Long = pair._2
-        f"$avgUrl%d 1 => avg unique per session $avgUrl%s\n"
+      def reduceTwoPlus(ds: Dataset[(Long, Long)]): String = {
+        val (sessionCount: Long, urlCount: Long) = ds.reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+        format(urlCount, sessionCount)
       }
+      def reduceOne(pair: (Long, Long)): String = format(urlCount = pair._2, sessionCount = 1)
       aggregate(theDatasets.uniqueUrlPerSessions, "Not enough data\n", reduceOne, reduceTwoPlus)
     }
 
     val durationLine: String = {
+      def format(avgDuration: Double): String = f"$avgDuration%6.6f seconds\n"
       def reduceTwoPlus(ds: Dataset[ClientDurationAndSize]): String = {
         val durationsPerSessions = ds
           .map { _.durationAndSize}
           .reduce((a, b) => DurationAndSize(a.duration + b.duration, a.size + b.size))
-        val avgDuration = durationsPerSessions.duration/(durationsPerSessions.size * billion)
-        f"$avgDuration%6.6f seconds\n"
+        format(durationsPerSessions.duration/(durationsPerSessions.size * billion))
       }
-      def reduceOne(d: ClientDurationAndSize): String = f"${d.durationAndSize.duration}%6.6f seconds\n"
+      def reduceOne(d: ClientDurationAndSize): String = format(d.durationAndSize.duration)
       aggregate(theDatasets.durationAndSizesPerSessionsDS, "Not enough data\n", reduceOne, reduceTwoPlus )
     }
 
@@ -148,6 +150,7 @@ object WeblogChallenge {
     saveAggregate("output/AvgDuration.txt", durationLine)
     saveToParquet(SaveToParquetParams(theDatasets.engagedClients, "output/EngagedClients", "engaged", 20 ))
     saveToParquet(SaveToParquetParams(theDatasets.sessionizedCounts, "output/SessionizedCounts", "Sessionized Count", 20 ))
+    println(s"Pipeline Log is $theLog}")
   }
 
 }
